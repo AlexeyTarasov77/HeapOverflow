821319493a170ec3903e456492cd316e0e3a4cc3
AlexeyTarasov77 <alexxey2707@gmail.com>
2024-11-16 12:45:58 +0100

    Added config loader from yaml config file

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { QuestionsModule } from './questions/questions.module';
+import { ConfigModule } from '@nestjs/config';
+import { configLoader } from './config';
 
 @Module({
-  imports: [QuestionsModule],
+  imports: [
+    QuestionsModule,
+    ConfigModule.forRoot({
+      load: [configLoader(process.env.CONFIG_PATH)],
+      isGlobal: true,
+    }),
+  ],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function start() {
+  if (!process.env.CONFIG_PATH) {
+    console.error('CONFIG_PATH is not set');
+    process.exit(1);
+  }
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<number>('server.port') ?? 3000;
+  const HOST = configService.get<string>('server.host') ?? 'localhost';
+  console.log(`Running server on http://${HOST}:${PORT}`);
+  await app.listen(PORT, HOST);
 }
 start();
