3dfd5f1c63f50b834377ef0ddd466ef78990aa32
AlexeyTarasov77 <alexxey2707@gmail.com>
2024-12-07 14:42:09 +0100

    FRONTEND: Added posts filtering

--- frontend/src/pages/posts-list/posts-list.ui.tsx
+++ frontend/src/pages/posts-list/posts-list.ui.tsx
@@ -1,11 +1,14 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { FOOTER_LINKS, POSTS, SIDEBAR_LINKS } from "../../app/constants";
+import { Badge } from "../../widgets";
+import { Select } from "../../widgets/select";
 
 interface IPostData {
   name: string;
   body: string;
   authorName: string;
   createdAt: Date;
+  category: string;
 }
 
 function Link({ children, href }: { children: ReactNode; href: string }) {
@@ -19,11 +22,20 @@ function Link({ children, href }: { children: ReactNode; href: string }) {
   );
 }
 
-function Post({ postData }: { postData: IPostData }) {
+function Post({
+  postData,
+  badgeOnClick
+}: {
+  postData: IPostData;
+  badgeOnClick?: React.MouseEventHandler<HTMLSpanElement>;
+}) {
   return (
     <div className="bg-orange-500 p-3 flex flex-col gap-3 rounded transition-colors hover:bg-orange-600">
       <div>
         <h3 className="text-white font-semibold text-2xl">{postData.name}</h3>
+      </div>
+      <div>
+        <Badge onClick={badgeOnClick}>{postData.category}</Badge>
       </div>
       <div>
         <p className="text-lg text-slate-300">{postData.body}</p>
@@ -45,6 +57,16 @@ function Post({ postData }: { postData: IPostData }) {
 }
 
 export function PostsList() {
+  const [filteredPosts, setFilteredPosts] = useState(POSTS);
+  const categoryOptions = new Set(POSTS.map(post => post.category));
+  const [currCategory, setCurrCategory] = useState("all");
+  useEffect(() => {
+    let filterRes = POSTS;
+    if (currCategory !== "all") {
+      filterRes = POSTS.filter(({ category }) => category === currCategory);
+    }
+    setFilteredPosts(filterRes);
+  }, [currCategory]);
   return (
     <>
       <header>
@@ -57,9 +79,35 @@ export function PostsList() {
         </div>
       </header>
       <main>
+        <div className="p-3 bg-slate-200 rounded-lg text-white max-w-fit mb-3">
+          <Select
+            selectedOptionName={currCategory}
+            onChange={e => {
+              setCurrCategory(e.target.value);
+            }}
+            labelText="Category"
+            options={[
+              ...Array.from(categoryOptions).map(category => ({
+                name: category,
+                value: category
+              })),
+              { name: "All", value: "all" }
+            ]}
+          />
+        </div>
         <div className="flex flex-col gap-5 p-3">
-          {POSTS.map(post => (
-            <Post postData={post} />
+          {filteredPosts.map(post => (
+            <Post
+              key={post.name}
+              postData={post}
+              badgeOnClick={e => {
+                setFilteredPosts(
+                  POSTS.filter(
+                    ({ category }) => category === e.currentTarget.innerText
+                  )
+                );
+              }}
+            />
           ))}
         </div>
       </main>
